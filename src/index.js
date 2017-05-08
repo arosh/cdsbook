3aeae93406549502d3d650fc7ef4176e3849cc12
Sho Iizuka <iizuka.sho.ij4@is.naist.jp>
2017-05-08 21:27:25 +0900

    ハリボテ

--- src/index.js
+++ src/index.js
@@ -2,26 +2,80 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ShannonFanoCode } from './services/ShannonFanoCode';
 
-const App = () => (
-  <div>
-    <table className="table table-hover">
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>Frequency</th>
-          <th>Code Length</th>
-          <th>Code</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th scope="row">1</th>
-          <td><input type="text" className="form-control" /></td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+const Item = ({ id, onChange, initialFreq, codeLength, code }) => (
+  <tr>
+    <th scope="row">{id}</th>
+    <td>
+      <input
+        type="text"
+        className="form-control"
+        defaultValue={initialFreq}
+        onChange={(e) => onChange(id, e.target.value)} />
+    </td>
+    <td>{codeLength}</td>
+    <td>{code}</td>
+  </tr>
 );
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.itemRefs = {}
+    this.onChange = this.onChange.bind(this);
+    // const frequencies = [600, 300, 50, 25, 25];
+    const frequencies = [1, 1, 1];
+    const sfc = new ShannonFanoCode(frequencies);
+    const codeLen = sfc.codeLength();
+    const codes = sfc.buildPrefixCode();
+    const items = [];
+    for (let i = 0; i < frequencies.length; i++) {
+      items.push({
+        id: i + 1,
+        initialFreq: frequencies[i],
+        codeLength: codeLen[i],
+        code: codes[i],
+      });
+    }
+    this.state = {
+      items,
+    }
+  }
+
+  render() {
+    // http://bootsnipp.com/snippets/featured/dynamic-table-row-creation-and-deletion
+    return (
+      <div>
+        <table className="table table-hover">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Frequency</th>
+              <th>Code Length</th>
+              <th>Code</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.items.map(item =>
+              <Item
+                key={item.id}
+                id={item.id}
+                onChange={this.onChange}
+                initialFreq={item.initialFreq}
+                codeLength={item.codeLength}
+                code={item.code} />
+            )}
+          </tbody>
+        </table>
+        <button type="button" className="btn btn-default pull-left">Add Row</button>
+        <button type="button" className="btn btn-default pull-right">Delete Row</button>
+      </div>
+    );
+  }
+
+  onChange(id, value) {
+    console.log(id, value);
+  }
+}
 
 ReactDOM.render(
   <App />,
--- src/services/ShannonFanoCode.js
+++ src/services/ShannonFanoCode.js
@@ -13,7 +13,7 @@ export class ShannonFanoCode {
         ls.push(0);
         return 0;
       } else {
-        ls.push(Math.ceil(Math.log2(this.count / freq)) | 0);
+        ls.push(Math.log2(this.count / freq));
       }
     }
     return ls;
@@ -22,18 +22,25 @@ export class ShannonFanoCode {
     const codeLen = this.codeLength();
     const S = [];
     for (let i = 0; i < this.frequences.length; i++) {
-      S.push({ s: i, l: codeLen[i] });
+      S.push({ s: i, l: Math.ceil(codeLen[i]) | 0 });
     }
     S.sort((x, y) => x.l - y.l);
     const codes = [];
-    codes[0] = 0;
+    codes[0] = this.binaryCode(0, S[0].l);
     for (let i = 1; i < S.length; i++) {
       codes[i] = (codes[i - 1] + 1) << (S[i].l - S[i - 1].l);
     }
     const retval = [];
     for (let i = 0; i < S.length; i++) {
-      retval[S[i].s] = codes[i].toString(2);
+      retval[S[i].s] = this.binaryCode(codes[i], S[i].l);
     }
     return retval;
+  }
+  binaryCode(value, length) {
+    let s = value.toString(2);
+    while (s.length < length) {
+      s = "0" + s;
+    }
+    return s;
   }
 }
